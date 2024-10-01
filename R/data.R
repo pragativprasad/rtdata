f74460db3dc8240a79b24afe24f533454c7c0975
Pragati Prasad <pragativprasad@gmail.com>
2024-10-01 12:48:34 -0400

    Initial commit

--- R/data.R
+++ R/data.R
@@ -2,16 +2,12 @@
 #'
 #' @contributors: Pragati V Prasad
 #'
-#' @source: GLEAM
+#' California_infections.rda
 #'
-#' @format
-#'
-#' {state_name}_infections.rda
-#'
-#' The time series files of the SLIR model simulation of a synthetic epidemic across 3 locations (California, North Carolina and Massachusetts).
+#' The time series of the SLIR model simulation of a synthetic epidemic across California.
 #'
 #'
-#' There are 10 stochastic realizations for each state/county.
+#' There are 10 stochastic realizations for each county.
 #'
 #' \describe{
 #'  \item{run_id}{stochastic realization id}
@@ -22,12 +18,14 @@
 #'  \item{geoid}{county id}
 #'  \item{new_"CompartmentName"}{daily number of new transitions into that compartment. new_Latent: number of S->L transitions on that day}
 #' }
+#'
+#' @source: GLEAM
 #' -----------------------------------------------
 #'
-#' {state_name}_hosp_linelist.rda
-#' The line list files report the hospitalizations of a synthetic epidemic across 3 locations (California, North Carolina and Massachusetts).
+#' California_hosp_linelist.rda
+#' The line list reports the hospitalizations of a synthetic epidemic in California.
 #'
-#' There are 10 stochastic realizations for each state.
+#' There are 10 stochastic realizations for each county.
 #'
 #' \describe{
 #'  \item{run_id}{stochastic realization id}
@@ -39,22 +37,100 @@
 #'  \item{reporting_date}{Date the hospitalization was reported in the reporting system}
 #' }
 #'
-#'
-#'#'-------------------------------------------------
-#'
-#'Weekly_US_Hospitalizations_20240925.rda
-#'
-#'\describe{
-#'  This is data from [CDC](https://data.cdc.gov/Public-Health-Surveillance/Weekly-United-States-Hospitalization-Metrics-by-Ju/aemt-mg7g/about_data)
-#'  Data was pulled on 2024-09-25
-#'}
-#'
+#' @source: GLEAM
 #'-------------------------------------------------
 #'
-#'Weekly_US_Deaths_20240924.rda
+#' Massachusetts_infections.rda
+#'
+#' The time series of the SLIR model simulation of a synthetic epidemic across Massachusetts
+#'
+#'
+#' There are 10 stochastic realizations for each county.
+#'
+#' \describe{
+#'  \item{run_id}{stochastic realization id}
+#'  \item{state_name}{name of state}
+#'  \item{county_full_name}{name of county}
+#'  \item{date}{date - daily}
+#'  \item{population}{population size in the county}
+#'  \item{geoid}{county id}
+#'  \item{new_"CompartmentName"}{daily number of new transitions into that compartment. new_Latent: number of S->L transitions on that day}
+#' }
+#'
+#' @source: GLEAM
+#' -----------------------------------------------
+#'
+#' Massachusetts_hosp_linelist.rda
+#' The line list reports the hospitalizations of a synthetic epidemic in Massachusetts.
+#'
+#' There are 10 stochastic realizations for each county.
+#'
+#' \describe{
+#'  \item{run_id}{stochastic realization id}
+#'  \item{indiv_idx}{individual id}
+#'  \item{state_name}{name of state}
+#'  \item{county_full_name}{name of county}
+#'  \item{infection_date}{Date of Infection: S -> L}
+#'  \item{hospitalization_date}{Date of hospitalization}
+#'  \item{reporting_date}{Date the hospitalization was reported in the reporting system}
+#' }
+#'
+#' @source: GLEAM
+#'-------------------------------------------------
+#' North_Carolina_infections.rda
+#'
+#' The time series of the SLIR model simulation of a synthetic epidemic across North Carolina
+#'
+#'
+#' There are 10 stochastic realizations for each county.
+#'
+#' \describe{
+#'  \item{run_id}{stochastic realization id}
+#'  \item{state_name}{name of state}
+#'  \item{county_full_name}{name of county}
+#'  \item{date}{date - daily}
+#'  \item{population}{population size in the county}
+#'  \item{geoid}{county id}
+#'  \item{new_"CompartmentName"}{daily number of new transitions into that compartment. new_Latent: number of S->L transitions on that day}
+#' }
+#'
+#' @source: GLEAM
+#' -----------------------------------------------
+#'
+#' @format North_Carolina_hosp_linelist.rda
+#' The line list reports the hospitalizations of a synthetic epidemic in North Carolina
+#'
+#' There are 10 stochastic realizations for each county.
+#'
+#' \describe{
+#'  \item{run_id}{stochastic realization id}
+#'  \item{indiv_idx}{individual id}
+#'  \item{state_name}{name of state}
+#'  \item{county_full_name}{name of county}
+#'  \item{infection_date}{Date of Infection: S -> L}
+#'  \item{hospitalization_date}{Date of hospitalization}
+#'  \item{reporting_date}{Date the hospitalization was reported in the reporting system}
+#' }
+#'
+#' @source: GLEAM
+#'-------------------------------------------------
+#'
+#'
+#' Weekly_US_Hospitalizations_20240925.rda
 #'
 #'\describe{
-#'  This is data from [CDC COVID-19 data tracker](https://covid.cdc.gov/covid-data-tracker/#maps_percent-covid-deaths)
+#'  US COVID-19 Hospital Admissions data, pulled on 2024-09-25
+#'}
+#'
+#' @source https://data.cdc.gov/Public-Health-Surveillance/Weekly-United-States-Hospitalization-Metrics-by-Ju/aemt-mg7g/about_data
+#'-------------------------------------------------
+#'
+#' Weekly_US_Deaths_20240924.rda
+#'
+#'\describe{
 #'  Data was pulled on 2024-09-24
 #'}
+#'
+#' @source https://covid.cdc.gov/covid-data-tracker/#maps_percent-covid-deaths
+#'
 
